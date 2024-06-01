2ccfd9080ef3576e345bf11d5dc9d00bdd0b2633
mariuskas1 <marius.kasparek@gmail.com>
2024-06-01 11:49:54 +0200

    added sidebar and header

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+function redirectTo(page){
+    window.location.href = "./help.html";
+}
+
+
+function openUserMenu(){
+    document.getElementById('user-menu').classList.remove('d-none');
+}
+
+function closeUserMenu(){
+    document.getElementById('user-menu').classList.add('d-none');
+}
