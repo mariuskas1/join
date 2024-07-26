3ef44f841a244113e57ae3c3ecb73ce15f3e24c6
mariuskas1 <marius.kasparek@gmail.com>
2024-07-26 10:31:25 +0200

    contacts new

--- js/contacts.js
+++ js/contacts.js
@@ -1,13 +1,15 @@
 const BASE_URL = "https://join-4544d-default-rtdb.europe-west1.firebasedatabase.app";
 let currentUser;
 let contacts = [];
+let groupedContacts = {};
+let activeContact;
 
 
 
 document.addEventListener("DOMContentLoaded", async function(){
     setTimeout(activateLink, 100);
     await getCurrentUserData();
-    displayUserInitials();
+    setTimeout(displayUserInitials, 40);
     await loadContacts();
     displayContacts();
 });
@@ -57,13 +59,46 @@ function activateLink(){
 }
 
 
-
 function activateContact(div){
     document.querySelectorAll(".contacts-display-small").forEach(function(contact){
         contact.classList.remove("active-contact-sm");
         })
     div.classList.add("active-contact-sm");
+
+    activeContact = '';
+    let activeContactName = div.querySelector(".contact-name-sm").innerHTML;
+    activeContact = contacts.find(contact => contact.name === activeContactName);
+    displayActiveContact();
 }
+
+
+function displayActiveContact(){
+    let scdDiv = document.getElementById("single-contact-display-div");
+    scdDiv.innerHTML = '';
+    
+    if(activeContact){
+        scdDiv.innerHTML += `
+        <div class="single-contact-display-header">
+            <div class="scd-initials">${activeContact.initials}</div>
+            <div class="scd-name-div">
+                <span class="scd-name">${activeContact.name}</span>
+                <div class="scd-options-div">
+                    <button class="scd-option-btn" onclick="displayEditContactModal()"><img src="assets/img/edit.png"> Edit</button>
+                    <button class="scd-option-btn" onclick="deleteContact()"><img src="assets/img/delete.png"> Delete</button>
+                </div>
+            </div>
+        </div>
+        <div class="single-contact-display-body">
+            <span class="contact-info">Contact Information</span>
+            <span class="scd-info-title">Email</span>
+            <span class="contact-mail">${activeContact.mail}</span>
+            <span class="scd-info-title">Phone</span>
+            <span class="contact-phone">${activeContact.phone}</span>
+        </div>
+        `;
+    }
+}
+
 
 
 function displayAddContactModal(){
@@ -91,6 +126,8 @@ async function uploadNewContact(){
         form.reset();
         hideAddContactModal();
         displayConfirmationMessage();
+        await loadContacts();
+        displayContacts();
     } else {
         console.log("Failed to create a new contact. Please check the form inputs.");
     }
@@ -118,10 +155,18 @@ function createNewContact(){
         newContact = {
             "name": name.value,
             "mail": mail.value,
-            "phone": phone.value
+            "phone": phone.value,
+            "initials": getContactInitials(name.value),
+            "info": "Contact Information"
         }
         return newContact;
     }
+}
+
+
+function getContactInitials(name){
+    let initials = name.split(/\s+/).map(part => part[0]).join('');
+    return initials.toUpperCase();
 }
 
 
@@ -135,16 +180,147 @@ function displayConfirmationMessage(){
 }
 
 
-
 async function loadContacts(){
     let response = await fetch(BASE_URL + "/allContacts/" + currentUser + ".json");
-    contacts = await response.json();
-    console.log(contacts);
+    let responseToJson = await response.json();
+    if( !responseToJson) {
+        console.log("No existing contacts yet")
+    } else {
+        contacts = Object.values(responseToJson);
+    }
 }
 
 
 
 function displayContacts(){
+    let contactsContainer = document.getElementById("contacts-display-bar");
+    contactsContainer.innerHTML = '';
 
+    sortContacts();
+
+    for (let letter in groupedContacts) {
+        if (groupedContacts.hasOwnProperty(letter)) {
+            const group = groupedContacts[letter];
+            let groupHTML = `
+                <div class="contacts-group">
+                    <div class="contacts-group-letter">${letter}</div>
+                    <div class="contacts-display-bar">`;
     
+            for (let i = 0; i < group.length; i++) {
+                const contact = group[i];
+                groupHTML += `
+                    <div class="contacts-display-small" onclick="activateContact(this)">
+                        <div class="initials-div">${contact.initials}</div>
+                        <div class="contacts-display-info">
+                            <div class="contact-name-sm">${contact.name}</div>
+                            <div class="contact-mail-sm">${contact.mail}</div>
+                        </div>
+                    </div>`;
+            }
+
+            groupHTML += `</div></div>`;
+            contactsContainer.innerHTML += groupHTML;
+        }
+    }
+}
+
+
+function sortContacts(){
+    groupedContacts = {};
+    
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        const firstLetter = contact.name.charAt(0).toUpperCase();
+        if (!groupedContacts[firstLetter]) {
+            groupedContacts[firstLetter] = [];
+        }
+        groupedContacts[firstLetter].push(contact);
+    }
+
+}
+
+
+function displayEditContactModal(){
+    displayAddContactModal();
+
+    document.getElementById("add-contact-modal-title").innerHTML = "Edit Contact";
+    document.getElementById("add-contact-team").innerHTML = "";
+    document.getElementById("new-contact-name").value = activeContact.name;
+    document.getElementById("new-contact-mail").value = activeContact.mail;
+    document.getElementById("new-contact-phone").value = activeContact.phone;
+
+    document.getElementById("cancel-or-delete-btn").innerHTML = "Delete";
+    document.getElementById("cancel-or-delete-btn").onclick = deleteContact;
+
+    document.getElementById("create-or-save-btn").innerHTML = "Save" + `<img class="btn-icon" src="assets/img/check_white.png">`;
+    document.getElementById("create-or-save-btn").type = "button";
+    document.getElementById("create-or-save-btn").onclick = editContact;
+ }
+
+
+function editContact(){
+
+
+}
+
+
+
+async function deleteContact(){
+    deleteContactLocally();
+    changeActiveContact();
+    await deleteContactsOnServer();
+    await uploadContactsOnServer();
+    displayActiveContact();
+    displayContacts();
+    changeActiveContactDisplaySmall();
+}
+
+
+
+function changeActiveContact(){
+    let currentIndex = contacts.findIndex(contact => contact.mail === activeContact.mail);
+
+    let nextIndex = (currentIndex + 1) % contacts.length;
+    activeContact = contacts[nextIndex];
+}
+
+
+function deleteContactLocally(){
+    contacts = contacts.filter(contact => contact.mail !== activeContact.mail);
+}
+
+
+async function deleteContactsOnServer(){
+    let response = await fetch(BASE_URL + "/allContacts/" + currentUser + ".json",{
+        method: "DELETE",
+    });
+    return responseToJson = await response.json();
+}
+
+
+async function uploadContactsOnServer(){
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        let existingContact = {
+            "name": contact.name,
+            "mail": contact.mail,
+            "phone": contact.phone,
+            "initials": getContactInitials(contact.name),
+            "info": "Contact Information"
+        }
+        await postData("/allContacts/" + currentUser.trim(), existingContact);
+    }
+}
+
+
+function changeActiveContactDisplaySmall() {
+    const contactDivs = document.querySelectorAll('.contacts-display-small');
+    contactDivs.forEach(div => {
+        const nameDiv = div.querySelector('.contact-name-sm');
+        if (nameDiv && nameDiv.innerHTML === activeContact.name) {
+            div.classList.add("active-contact-sm");
+        }
+    });
 }
